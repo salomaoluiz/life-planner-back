4e38bf4550fc461c7abcde7574e0a28a6f8821a7
salomao-neto <salomao.cruz.15@gmail.com>
2025-12-04 13:49:40 -0300

    chore: update user controller to remove versioning and configure routing for v1 module

--- src/api/v1/v1.module.ts
+++ src/api/v1/v1.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 
 import { UserModule } from '@api/v1/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    RouterModule.register([
+      {
+        module: UserModule,
+        path: 'v1',
+      },
+    ]),
+  ],
 })
 export class V1Module {}
