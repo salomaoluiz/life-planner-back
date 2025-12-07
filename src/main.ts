ec3b1e3b17b96317b9054758d85634ac4f382f84
salomao-neto <salomao.cruz.15@gmail.com>
2025-12-07 16:05:40 -0300

    chore: add Swagger support for API documentation and update package dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder().setVersion('1.0').addBearerAuth().build();
+
+  function documentFactory() {
+    return SwaggerModule.createDocument(app, config);
+  }
+
   app.enableVersioning({
     type: VersioningType.URI,
   });
   app.setGlobalPrefix('api');
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
